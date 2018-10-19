b6e328712f23349103e824873b309ccb9fd5f848
Gareth Rendle <gareth.rendle@uni-weimar.de>
2018-10-19 12:29:13 +0200

    made a class

---
+++ Ex1/ex1.py
@@ -0,0 +1,15 @@
+class TMatrix:
+
+	#sets unity matrix
+	def __init__(self):
+		self.values = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+		print(self.values)
+
+	#def __init__(self):
+
+	#def mult(self, other_matrix):
+
+
+
+mat1 = TMatrix();
+
